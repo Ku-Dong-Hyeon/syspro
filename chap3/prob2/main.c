118eae8e0a9e992cc90fa82cf494473ee8db9745
Ku-Dong-Hyeon <wlakfldh123@cu.ac.kr>
2023-10-12 03:53:00 +0000

    test2

--- chap3/prob2/main.c
+++ chap3/prob2/main.c
@@ -1,30 +1,27 @@
 #include <stdio.h>
 #include <string.h>
 #include "copy.h"
-char line[MAXLINE];
+char line[5][MAXLINE];
 char longest[MAXLINE];
 
 int main(){
-	int len;
-	int max;
-	max = 0;
-
-	while(fgets(line,MAXLINE,stdin) != NULL) {
-		len = strlen(line);
-
-		if(len > max) {
-			max = len;
-			copy(line, longest);
+	int count = 0;
+	
+	while(count < 5) {
+		if(fgets(line[count],MAXLINE,stdin) == NULL) {
+			break;
 		}
+		longest[count] = strlen(line[count]);
+		count++;
 	}
 
 	for(int i=0; i<4; i++){
 		for(int j=0; j<4-i; j++){
-			if(longest[j]<longest[j+i]){
+			if(longest[j]<longest[j+1]){
 				char temp[MAXLINE];
-				strcpy(temp, line[j]);
-				strcpy(line[j],line[j+1]);
-				strcpy(line[j+1],temp);
+				strncpy(temp, line[j], MAXLINE);
+				strncpy(line[j],line[j+1],MAXLINE);
+				strncpy(line[j+1],temp,MAXLINE);
 
 				int templen = longest[j];
 				longest[j] = longest[j+1];
@@ -33,8 +30,8 @@ int main(){
 		}
 	}
 
-	if (max > 0)
-		printf("%s", longest);
+	for(int i = 0; i<5; i++)
+		printf("%s", line[i]);
 
 	return 0;
 }
