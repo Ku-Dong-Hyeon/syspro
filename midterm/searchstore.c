211f41cf1e45a032c760f486603ced1de5613882
Ku-Dong-Hyeon <wlakfldh123@cu.ac.kr>
2023-10-26 07:01:29 +0000

    midterm

--- midterm/searchstore.c
+++ midterm/searchstore.c
@@ -11,22 +11,24 @@ struct menu {
 int main(int argc, char* argv[]) {
 	struct menu rec;
 	char c[20];
+	char x[20] = "coffee";
+	char y[20] = "tea";
 	printf("input goods category ] ");
 	scanf("%s\n",c);
-	if(strcmp(c,"coffee")==0){
+	if(strcmp(c,x)==0){
 		FILE *fp = fopen("db.dat","r");
 		printf("%s %-7s %-4s %-4s %-4s\n","id","name","category","expired date","stock");
 		while(fscanf(fp,"%d %s %s %d %d",&rec.id,rec.name,rec.category,&rec.date,&rec.stock)==5)
-			if(rec.category == c){
+			if(rec.category == x){
 				printf("%d %6s %3s %3d %3d\n",rec.id,rec.name,rec.category,rec.date,rec.stock);
 			}
 		fclose(fp);
 	}
-	else if (strcmp(c,"tea")==0){
+	else if (strcmp(c,y)==0){
 		FILE *fp = fopen("db.dat","r");
 		printf("%s %-7s %-4s %-4s %-4s\n","id","name","category","expired date","stock");
 		while(fscanf(fp,"%d %s %s %d %d",&rec.id,rec.name,rec.category,&rec.date,&rec.stock)==5)
-			if(rec.category == c){
+			if(rec.category == y){
 				printf("%d %6s %3s %3d %3d\n",rec.id,rec.name,rec.category,rec.date,rec.stock);
 			}
 		fclose(fp);
